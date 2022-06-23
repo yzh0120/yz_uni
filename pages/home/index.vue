fe30727a2322414b76f8a08243801524dd9c7061
yzh0120 <644193385@qq.com>
2022-06-23 14:52:17 +0800

    up

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -8,47 +8,11 @@
 export default {
 	data() {
 		return {
-			showSex: false,
-			model1: {
-				userInfo: {
-					name: 'uView UI',
-					sex: '',
-				},
-			},
-			actions: [{
-				name: '男',
-				},
-				{
-					name: '女',
-				},
-				{
-					name: '保密',
-				},
-			],
-			rules: {
-				'userInfo.name': {
-					type: 'string',
-					required: true,
-					message: '请填写姓名',
-					trigger: ['blur', 'change']
-				},
-				'userInfo.sex': {
-					type: 'string',
-					max: 1,
-					required: true,
-					message: '请选择男或女',
-					trigger: ['blur', 'change']
-				},
-			},
-			radio: '',
-			switchVal: false
+
 		};
 	},
 	methods: {
-		sexSelect(e) {
-			this.model1.userInfo.sex = e.name
-			this.$refs.form1.validateField('userInfo.sex')
-		},
+
 	},
 };
 </script>
